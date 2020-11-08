82046cfe9c5a656df3d5037094af128eeb934cbd
Ryan Smith <rysmith2113@gmail.com>
2020-11-08 12:23:41 -0500

    Add a file organization notice

--- src/config_src/permissions.py
+++ src/config_src/permissions.py
@@ -11,6 +11,7 @@ from typing import NoReturn
 
 from .config import Config
 
+# NOTE: In the futre, this class may be turned into a module with two functions
 class Permissions:
 	""" Holds Quinton's permission settings and the methods that set them. """
 
