2797752f3b2bbee983ffe72db8c602d9f2efe37b
Ryan Smith <rysmith2113@gmail.com>
2020-11-23 23:57:49 -0500

    Add a way to collect ToolKits from the source tree/filesystem

---
+++ src/tk_src/fetcher.py
@@ -0,0 +1,28 @@
+#
+# FILENAME: fetcher.py | Quinton-VoiceAssistant
+# DESCRIPTION: Collects ToolKit files from the source tree.
+# CREATED: 2020-11-23 @ 11:09 PM
+# COPYRIGHT: Copyright (c) 2020 by Ryan Smith <rysmith2113@gmail.com>
+#
+
+import subprocess
+from pathlib import Path
+from importlib import import_module
+
+def fetch() -> list:
+	""" Collect all non-blacklisted ToolKits. """
+	# For more info on blacklisting ToolKits, see `../doc/excluding-toolkits.md`.
+
+	# Get a list of all the installed ToolKits
+	TK_LIST = [tk for tk in subprocess.check_output("ls ../../data/toolkits", shell=True).decode("utf-8").strip().split("\n")]
+
+	EXCLUDE_FILE = Path("../../data/config/toolkit-exclude.txt") # The list of ToolKits to exclude
+
+	toolkits = list()
+
+	with open(str(EXCLUDE_FILE), "r") as blacklist:
+		for tk in TK_LIST:
+			if not tk in blacklist.read().split("\n"):
+				toolkits.append(f"data.toolkits.{tk.strip('.py')}")
+	
+	return toolkits
