7b9df3cd2d3998aefae41721f194305f285dcbba
Ryan Smith <rysmith2113@gmail.com>
2020-12-12 13:43:52 -0500

    Undo the change to move it to branch v0.2.1-dev

--- src/voiceassistant.py
+++ src/voiceassistant.py
@@ -699,16 +699,18 @@ class VoiceAssistant:
 			"I'm unable to do that"
 		]
 
-		USABLE_REPLIES = {
-			0: None,
-			1: [2, 3, 5],
-			2: [2, 3, 7],
-			3: [4],
-			4: [0],
-			5: [6, 8]
-		}
-
-		usable_replies = USABLE_REPLIES.get(commandID)
+		if commandID == 0:
+			pass
+		elif commandID == 1:
+			usable_replies = [2, 3, 5]
+		elif commandID == 2:
+			usable_replies = [2, 3, 7]
+		elif commandID == 3:
+			usable_replies = [4]
+		elif commandID == 4:
+			usable_replies = [0]
+		elif commandID == 5:
+			usable_replies = [6, 8]
 
 		# Make sure all timestamps are up to date
 		try:
@@ -724,7 +726,7 @@ class VoiceAssistant:
 		except TypeError:
 			raise TimestampError	
 
-		if response is usable_replies is None:
+		if (response is None) and (usable_replies == list()):
 			if sys.version_info.minor > 8:
 					random.seed()
 			else:
