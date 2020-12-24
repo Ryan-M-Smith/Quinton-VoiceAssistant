29cbf28c3ce7d2eb7d1cd8a27a223118b0adf7ed
Ryan Smith <rysmith2113@gmail.com>
2020-12-23 19:29:45 -0500

    Add a dependency installation script

---
+++ dep-install.sh
@@ -0,0 +1,44 @@
+# FILENAME: dep-install.sh
+# DESCRIPTION: Install non-Python dependencies for various Linux distributions.
+#
+# This script currently supports `apt-get`, `yum`, `dnf`, and `brew` (Homebrew).
+#
+
+# Install dependencies
+function dep_install { 
+	if [ -f /usr/bin/apt-get ]; then # apt-get
+		xargs sudo apt-get install --assume-yes < pkglist-apt-brew.txt
+	elif [ -f /usr/bin/yum ]; then # YUM
+		xargs sudo yum install --assumeyes < pkglist-yum-dnf.txt
+	elif [ -f /usr/bin/dnf ]; then # DNF
+		xargs sudo dnf install --assumeyes < pkglist-yum-dnf.txt
+	elif [[ $(/usr/local/ | grep -c brew) > 0 || \
+			$(/opt/homebrew/ | grep -c brew) > 0 || \
+			$(/usr/bin/ | grep -c brew) > 0 ]]		# Homebrew
+	then
+		xargs brew install < pkglist-apt-brew.txt
+	fi
+}
+
+# Install dependencies
+function dep_uninstall {
+	if [ -f /usr/bin/apt-get ]; then # apt-get
+		xargs sudo apt-get uninstall --assume-yes < pkglist-apt-brew.txt
+	elif [ -f /usr/bin/yum ]; then # YUM
+		xargs sudo yum uninstall --assumeyes < pkglist-yum-dnf.txt
+	elif [ -f /usr/bin/dnf ]; then # DNF
+		xargs sudo dnf uninstall --assumeyes < pkglist-yum-dnf.txt
+	elif [[ $(/usr/local/ | grep -c brew) > 0 || \
+			$(/opt/homebrew/ | grep -c brew) > 0 || \
+			$(/usr/bin/ | grep -c brew) > 0 ]]		# Homebrew
+	then
+		xargs brew uninstall < pkglist-apt-brew.txt
+	fi
+}
+
+# Run everything
+if [[ $0 == "install" || $0 == "" ]]; then
+	dep_install
+else
+	dep_uninstall
+fi
