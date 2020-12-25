0e4a277bd2f3f5329d72cc064422453d4dc14983
Ryan Smith <rysmith2113@gmail.com>
2020-12-24 23:12:15 -0500

    Use the correct path to the package lists

--- dep-manager.sh
+++ dep-manager.sh
@@ -7,39 +7,39 @@
 # Install dependencies
 function dep_install { 
 	if [ -f /usr/bin/apt-get ]; then # apt-get
-		xargs sudo apt-get install --assume-yes < pkglist-apt-brew.txt
+		xargs sudo apt-get install --assume-yes < pkglists/pkglist-apt-brew.txt
 	elif [ -f /usr/bin/yum ]; then # YUM
-		xargs sudo yum install --assumeyes < pkglist-yum-dnf.txt
+		xargs sudo yum install --assumeyes <  pkglists/pkglist-yum-dnf.txt
 	elif [ -f /usr/bin/dnf ]; then # DNF
-		xargs sudo dnf install --assumeyes < pkglist-yum-dnf.txt
+		xargs sudo dnf install --assumeyes <  pkglists/pkglist-yum-dnf.txt
 	elif [[ $(/usr/local/ | grep -c brew) > 0 || \
 			$(/opt/homebrew/ | grep -c brew) > 0 || \
 			$(/usr/bin/ | grep -c brew) > 0 ]]		# Homebrew
 	then
-		xargs brew install < pkglist-apt-brew.txt
+		xargs brew install <  pkglists/pkglist-apt-brew.txt
 	fi
 }
 
 # Uninstall dependencies
 function dep_uninstall {
 	if [ -f /usr/bin/apt-get ]; then # apt-get
-		xargs sudo apt-get uninstall --assume-yes < pkglist-apt-brew.txt
+		xargs sudo apt-get uninstall --assume-yes <  pkglists/pkglist-apt-brew.txt
 	elif [ -f /usr/bin/yum ]; then # YUM
-		xargs sudo yum uninstall --assumeyes < pkglist-yum-dnf.txt
+		xargs sudo yum uninstall --assumeyes <  pkglists/pkglist-yum-dnf.txt
 	elif [ -f /usr/bin/dnf ]; then # DNF
-		xargs sudo dnf uninstall --assumeyes < pkglist-yum-dnf.txt
+		xargs sudo dnf uninstall --assumeyes <  pkglists/pkglist-yum-dnf.txt
 	elif [[ $(/usr/local/ | grep -c brew) > 0 || \
 			$(/opt/homebrew/ | grep -c brew) > 0 || \
 			$(/usr/bin/ | grep -c brew) > 0 ]]		# Homebrew
 	then
-		xargs brew uninstall < pkglist-apt-brew.txt
+		xargs brew uninstall <  pkglists/pkglist-apt-brew.txt
 	fi
 }
 
 # Run everything
-if [[ $0 == "install" ]]; then
+if [[ $1 == "install" ]]; then
 	dep_install
-elif [[ $0 == "uninstall" ]]; then
+elif [[ $1 == "uninstall" ]]; then
 	dep_uninstall
 fi
 
